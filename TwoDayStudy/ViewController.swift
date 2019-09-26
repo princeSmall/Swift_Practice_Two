858479e1507c8f78665a332366dc740e779c6c94
le tong <le.tong@ly.com>
2019-09-26 09:54:19 +0800

    Initial Commit

---
+++ TwoDayStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoDayStudy
+//
+//  Created by le tong on 2019/9/26.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
